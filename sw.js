8e5e3753ead85ff3733a4ef2d9907ceda8279c65
ROCGMC <a0960730188@gmail.com>
2026-02-23 23:49:58 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,34 @@
-const CACHE_NAME = 'rms-v3-cache';
+const CACHE_NAME = 'rms-v4-cache';
 const ASSETS = [
-  'index.html',
-  'manifest.json',
-  'icon.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
 ];
 
+// 安裝並快取資源
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('V4 Caching assets');
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+// 激活並清理舊版快取
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
   );
 });
 
+// 攔截請求
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then(res => res || fetch(e.request))
